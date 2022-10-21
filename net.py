748fee5082fc7698f14a3790fca69b624c81c797
wally <lekalelhouari777@gmail.com>
2022-10-21 12:06:01 +0200

    backup

---
+++ net.py
@@ -0,0 +1,21 @@
+'''Socket Programming in * Python *'''
+
+import socket
+import sys
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("socket succesfully created!")
+except socket.error as err:
+    print(f'socket creation failed with error {err}')
+
+port = 80
+
+try:
+    host_ip = socket.gethostbyname('www.nasa.org')
+except socket.gaierror:
+    print('error resolving the host')
+    sys.exit()
+s.connect((host_ip, port))
+print(f'Socket has succesfully connected to Nasa.org port == {host_ip}')
+    
